986004e5b24ac994ae1d71382114ba85d452bf9c
Werliton Silva <werliton.silva@osf.digital>
2025-08-21 09:10:51 -0300

    feat: add outcome graph

--- src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
+++ src/components/BalancoFinanceiro/BalancoFinanceiro.jsx
@@ -1,7 +1,28 @@
+import { Pie } from "react-chartjs-2";
 import Cartao from "../Cartao/Cartao";
 import CartaoCabecalho from "../Cartao/CartaoCabecalho/CartaoCabecalho";
 import CartaoCorpo from "../Cartao/CartaoCorpo/CartaoCorpo";
 import styled from "styled-components";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { useRecoilValue } from "recoil";
+import { selectTransactionsToGraph } from "src/service/selectors/graphSelector";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  ArcElement,
+  Tooltip,
+  Legend
+);
 
 export const AreaChart = styled.div`
   padding: var(--padding-xs);
@@ -9,12 +30,51 @@ export const AreaChart = styled.div`
   height: 50%;
 `;
 
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "right",
+    },
+  },
+};
+
 const BalancoFinanceiro = () => {
+  const { categorias, despesas } = useRecoilValue(selectTransactionsToGraph);
+
   return (
     <Cartao>
       <CartaoCabecalho>Gastos por categoria</CartaoCabecalho>
       <CartaoCorpo>
-        <AreaChart></AreaChart>
+        <AreaChart>
+          <Pie
+            data={{
+              labels: categorias,
+              datasets: [
+                {
+                  label: "Gastos",
+                  data: despesas,
+                  backgroundColor: [
+                    "rgba(255, 99, 132, 0.2)",
+                    "rgba(54, 162, 235, 0.2)",
+                    "rgba(255, 206, 86, 0.2)",
+                    "rgba(75, 192, 192, 0.2)",
+                    "rgba(153, 102, 255, 0.2)",
+                  ],
+                  borderColor: [
+                    "rgba(255, 99, 132, 1)",
+                    "rgba(54, 162, 235, 1)",
+                    "rgba(255, 206, 86, 1)",
+                    "rgba(75, 192, 192, 1)",
+                    "rgba(153, 102, 255, 1)",
+                  ],
+                  borderWidth: 1,
+                },
+              ],
+            }}
+            options={options}
+          />
+        </AreaChart>
       </CartaoCorpo>
     </Cartao>
   );
